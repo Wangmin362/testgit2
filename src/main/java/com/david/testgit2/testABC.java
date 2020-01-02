f755cd1a88fc41860f61e22d62849ad4224ce47d
david <1614586246@qq.com>
2020-01-02 17:07:04 +0800

    滚滚长江东逝水

---
+++ src/main/java/com/david/testgit2/testABC.java
@@ -0,0 +1,15 @@
+package com.david.testgit2;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author david
+ * @create 2020-01-02 17:05
+ **/
+@Configuration
+public class testABC {
+
+    public void test(){
+
+    }
+}
